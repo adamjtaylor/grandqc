217270f2cd6d42b1533571edcc02f50ab663a804
Adam Taylor <adam.taylor@sagebase.org>
2025-02-21 21:03:02 +0000

    Fix Grandqc for nextflow

--- 01_WSI_inference_OPENSLIDE_QC/main.py
+++ 01_WSI_inference_OPENSLIDE_QC/main.py
@@ -3,6 +3,7 @@ Comments to version:
 - Uses tissue maps from tissue detector. Therefore, slides should be processed by tissue detector firstly.
 - Consider adding color schema if you use the tool for a new entity
 """
+
 from wsi_colors import colors_QC7 as colors
 import torch
 import argparse
@@ -15,34 +16,59 @@ from wsi_maps import make_overlay
 import numpy as np
 import timeit
 import cv2
+
 Image.MAX_IMAGE_PIXELS = 1000000000
 
-
 # DEVICE
-DEVICE = 'cuda'
-'''
+DEVICE = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps" if torch.backends.mps.is_available() else "cpu"
+)
+"""
 'cuda:0' - NVIDIA GPU card
 'mps'    - APPLE Silicon
-'''
+'cpu'    - Default to CPU if no GPU is available
+"""
+
+print(f"Device: {DEVICE}")
 
 # MODEL(S)
-MODEL_QC_DIR = './models/qc/'
-MODEL_QC_NAME = 'GrandQC_MPP15.pth'
+# MODEL_QC_DIR = "./models/qc/"
+MODEL_QC_NAME = "GrandQC_MPP15.pth"
 MPP_MODEL = 1.5
 M_P_S_MODEL = 512
-ENCODER_MODEL = 'timm-efficientnet-b0'
-ENCODER_MODEL_WEIGHTS = 'imagenet'
+ENCODER_MODEL = "timm-efficientnet-b0"
+ENCODER_MODEL_WEIGHTS = "imagenet"
 
 # CLASSES
 BACK_CLASS = 7
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--slide_folder', dest='slide_folder', help='path to WSIs', type=str)
-parser.add_argument('--output_dir', dest='output_dir', help='path to output folder', type=str)
-parser.add_argument('--start', dest='start', default=0,  help='start num of WSIs', type=int)
-parser.add_argument('--end', dest='end', default=-1, help='end num of WSIs', type=int)
-parser.add_argument('--ol_factor', dest='ol_factor', default=10,
-                    help='reduction factor of the overlay compared to dimensions of original WSI', type=int)
+parser.add_argument(
+    "--model_dir",
+    dest="MODEL_QC_DIR",
+    help="path to the model directory",
+    type=str,
+    default="./models/qc/",
+)
+parser.add_argument(
+    "--slide_folder", dest="slide_folder", help="path to WSIs", type=str
+)
+parser.add_argument(
+    "--output_dir", dest="output_dir", help="path to output folder", type=str
+)
+parser.add_argument(
+    "--start", dest="start", default=0, help="start num of WSIs", type=int
+)
+parser.add_argument("--end", dest="end", default=-1, help="end num of WSIs", type=int)
+parser.add_argument(
+    "--ol_factor",
+    dest="ol_factor",
+    default=10,
+    help="reduction factor of the overlay compared to dimensions of original WSI",
+    type=int,
+)
 
 args = parser.parse_args()
 
@@ -51,13 +77,16 @@ end = args.end
 SLIDE_DIR = args.slide_folder
 OUTPUT_DIR = args.output_dir
 OVERLAY_FACTOR = args.ol_factor
+MODEL_QC_DIR = args.MODEL_QC_DIR
 
 if end == -1:
     end = len(os.listdir(SLIDE_DIR))
 
 case_name = os.path.basename(OUTPUT_DIR)
-REPORT_FILE_NAME = f'report_{case_name}_' + str(start) + '_' + str(end)     # File name, ".txt" will be added in the end
-REPORT_OUTPUT_DIR = OUTPUT_DIR # where to save the text report
+REPORT_FILE_NAME = (
+    f"report_{case_name}_" + str(start) + "_" + str(end)
+)  # File name, ".txt" will be added in the end
+REPORT_OUTPUT_DIR = OUTPUT_DIR  # where to save the text report
 
 
 # =============================================================================
@@ -81,16 +110,16 @@ results = open(path_result, "a+")
 results.write(output_header)
 results.close()
 
-maps_dir = os.path.join(OUTPUT_DIR, 'maps_qc')
-overlay_dir = os.path.join(OUTPUT_DIR, 'overlays_qc')
-mask_dir = os.path.join(OUTPUT_DIR, 'mask_qc')
+maps_dir = os.path.join(OUTPUT_DIR, "maps_qc")
+overlay_dir = os.path.join(OUTPUT_DIR, "overlays_qc")
+mask_dir = os.path.join(OUTPUT_DIR, "mask_qc")
 
 try:
     os.mkdir(maps_dir)
     os.mkdir(overlay_dir)
     os.mkdir(mask_dir)
 except Exception as e:
-    print('The target folders are already there ..')
+    print("The target folders are already there ..")
 
 # ====================================================================
 # MAIN SCRIPT
@@ -113,21 +142,45 @@ for slide_name in slide_names[start:end]:
         slide = open_slide(path_slide)
 
         # GET SLIDE INFO
-        p_s, patch_n_w_l0, patch_n_h_l0, mpp, w_l0, h_l0, obj_power = slide_info(slide, M_P_S_MODEL, MPP_MODEL)
+        p_s, patch_n_w_l0, patch_n_h_l0, mpp, w_l0, h_l0, obj_power = slide_info(
+            slide, M_P_S_MODEL, MPP_MODEL
+        )
 
         # LOAD TISSUE DETECTION MAP
-        tis_det_map = Image.open(os.path.join(OUTPUT_DIR, 'tis_det_mask', slide_name + '_MASK.png'))
-        '''
+        tis_det_map = Image.open(
+            os.path.join(OUTPUT_DIR, "tis_det_mask", slide_name + "_MASK.png")
+        ).convert("L")
+        """
         Tissue detection map is generated on MPP = 10
         This map is used for on-fly control of the necessity of model inference.
         Two variants: reduced version with perfect correlation or full version scaled to working MPP of the tumor detection model
         Classes: 0 - tissue, 1 - background
-        '''
+        """
 
-        tis_det_map_mpp = np.array(tis_det_map.resize((int(w_l0 * mpp / MPP_MODEL), int(h_l0 * mpp / MPP_MODEL)), Image.Resampling.LANCZOS))
-        map, full_mask = slide_process_single(model_prim, tis_det_map_mpp, slide, patch_n_w_l0, patch_n_h_l0, p_s,
-                                              M_P_S_MODEL, colors, ENCODER_MODEL,
-                                              ENCODER_MODEL_WEIGHTS, DEVICE, BACK_CLASS, MPP_MODEL, mpp, w_l0, h_l0)
+        tis_det_map_mpp = np.array(
+            tis_det_map.resize(
+                (int(w_l0 * mpp / MPP_MODEL), int(h_l0 * mpp / MPP_MODEL)),
+                Image.Resampling.LANCZOS,
+            )
+        )
+        map, full_mask = slide_process_single(
+            model_prim,
+            tis_det_map_mpp,
+            slide,
+            patch_n_w_l0,
+            patch_n_h_l0,
+            p_s,
+            M_P_S_MODEL,
+            colors,
+            ENCODER_MODEL,
+            ENCODER_MODEL_WEIGHTS,
+            DEVICE,
+            BACK_CLASS,
+            MPP_MODEL,
+            mpp,
+            w_l0,
+            h_l0,
+        )
 
         # Timer stop
         stop = timeit.default_timer()
@@ -143,7 +196,9 @@ for slide_name in slide_names[start:end]:
         # =============================================================================
         # 8. MAKE AND SAVE OVERLAY for C8: HEATMAP ON REDUCED AND CROPPED SLIDE CLON
         # =============================================================================
-        overlay = make_overlay(slide, map, p_s, patch_n_w_l0, patch_n_h_l0, OVERLAY_FACTOR)
+        overlay = make_overlay(
+            slide, map, p_s, patch_n_w_l0, patch_n_h_l0, OVERLAY_FACTOR
+        )
 
         del map
 
@@ -159,7 +214,13 @@ for slide_name in slide_names[start:end]:
         output_temp = slide_name + "\t" + str(obj_power) + "\t" + str(mpp) + "\t"
         output_temp = output_temp + str(patch_n_h_l0) + "\t" + str(patch_n_w_l0) + "\t"
         output_temp = output_temp + str(patch_n_h_l0 * patch_n_w_l0) + "\t"
-        output_temp = output_temp + str(patch_n_h_l0 * p_s) + "\t" + str(patch_n_w_l0 * p_s) + "\t"
+        output_temp = (
+            output_temp
+            + str(patch_n_h_l0 * p_s)
+            + "\t"
+            + str(patch_n_w_l0 * p_s)
+            + "\t"
+        )
 
         output_temp = output_temp + str(round((stop - start) / 60, 1))
 
--- 01_WSI_inference_OPENSLIDE_QC/wsi_tis_detect.py
+++ 01_WSI_inference_OPENSLIDE_QC/wsi_tis_detect.py
@@ -8,43 +8,62 @@ from PIL import Image
 import segmentation_models_pytorch as smp
 from wsi_tis_detect_helper_fx import get_preprocessing, make_class_map
 
+print(f"CV2 version: {cv2.__version__}")
 
 # DEVICE
-DEVICE = 'cuda'
-'''
-'cuda' - NVIDIA GPU card
+DEVICE = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps" if torch.backends.mps.is_available() else "cpu"
+)
+"""
+'cuda:0' - NVIDIA GPU card
 'mps'    - APPLE Silicon
-'''
+'cpu'    - Default to CPU if no GPU is available
+"""
+
+print(f"Device: {DEVICE}")
 
 # MODEL TISSUE DETECTION:
-MODEL_TD_DIR = './models/td/'
-MODEL_TD_NAME = 'Tissue_Detection_MPP10.pth'
+# MODEL_TD_DIR = "./models/td/"
+MODEL_TD_NAME = "Tissue_Detection_MPP10.pth"
 MPP_MODEL_TD = 10
 M_P_S_MODEL_TD = 512
-ENCODER_MODEL_TD = 'timm-efficientnet-b0'
-ENCODER_MODEL_TD_WEIGHTS = 'imagenet'
+ENCODER_MODEL_TD = "timm-efficientnet-b0"
+ENCODER_MODEL_TD_WEIGHTS = "imagenet"
 
 # OVERLAY PARAMETERS (TRANSPARENCY)
-OVER_IMAGE = 0.7    # % original image
-OVER_MASK = 0.3     # % segmentation mask
+OVER_IMAGE = 0.7  # % original image
+OVER_MASK = 0.3  # % segmentation mask
 
 # COLORS for MASK
-colors = [[50, 50, 250],    # BLUE: TISSUE
-          [128, 128, 128]]  # GRAY: BACKGROUND
+colors = [[50, 50, 250], [128, 128, 128]]  # BLUE: TISSUE  # GRAY: BACKGROUND
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--slide_folder', dest='slide_folder', help='path to WSIs', type=str)
-parser.add_argument('--output_dir', dest='output_dir', help='path to output folder', type=str)
+parser.add_argument(
+    "--model_dir",
+    dest="MODEL_TD_DIR",
+    help="path to the model directory",
+    type=str,
+    default="./models/qc/",
+)
+parser.add_argument(
+    "--slide_folder", dest="slide_folder", help="path to WSIs", type=str
+)
+parser.add_argument(
+    "--output_dir", dest="output_dir", help="path to output folder", type=str
+)
 args = parser.parse_args()
 
 SLIDE_DIR = args.slide_folder
 OUTPUT_DIR = args.output_dir
+MODEL_TD_DIR = args.MODEL_TD_DIR
 
 # Create output dirs
-tis_det_dir_mask = os.path.join(OUTPUT_DIR, 'tis_det_mask/')
-tis_det_dir_over = os.path.join(OUTPUT_DIR, 'tis_det_overlay/')
-tis_det_dir_thumb = os.path.join(OUTPUT_DIR, 'tis_det_thumbnail/')
-tis_det_dir_mask_col = os.path.join(OUTPUT_DIR, 'tis_det_mask_col/')
+tis_det_dir_mask = os.path.join(OUTPUT_DIR, "tis_det_mask/")
+tis_det_dir_over = os.path.join(OUTPUT_DIR, "tis_det_overlay/")
+tis_det_dir_thumb = os.path.join(OUTPUT_DIR, "tis_det_thumbnail/")
+tis_det_dir_mask_col = os.path.join(OUTPUT_DIR, "tis_det_mask_col/")
 
 try:
     os.makedirs(tis_det_dir_mask)
@@ -52,12 +71,16 @@ try:
     os.makedirs(tis_det_dir_thumb)
     os.makedirs(tis_det_dir_mask_col)
 except:
-    print('The folders are already there ..')
+    print("The folders are already there ..")
 
 # Get slide names
-slide_names = sorted([f for f in os.listdir(SLIDE_DIR) if os.path.isfile(os.path.join(SLIDE_DIR, f))])
+slide_names = sorted(
+    [f for f in os.listdir(SLIDE_DIR) if os.path.isfile(os.path.join(SLIDE_DIR, f))]
+)
 
-preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER_MODEL_TD, ENCODER_MODEL_TD_WEIGHTS)
+preprocessing_fn = smp.encoders.get_preprocessing_fn(
+    ENCODER_MODEL_TD, ENCODER_MODEL_TD_WEIGHTS
+)
 
 model = smp.UnetPlusPlus(
     encoder_name=ENCODER_MODEL_TD,
@@ -66,7 +89,9 @@ model = smp.UnetPlusPlus(
     activation=None,
 )
 
-model.load_state_dict(torch.load(os.path.join(MODEL_TD_DIR, MODEL_TD_NAME), map_location='cpu'))
+model.load_state_dict(
+    torch.load(os.path.join(MODEL_TD_DIR, MODEL_TD_NAME), map_location="cpu")
+)
 model.to(DEVICE)
 model.eval()
 
@@ -82,17 +107,24 @@ for slide_name in slide_names:
         mpp = round(float(slide.properties["openslide.mpp-x"]), 4)
         reduction_factor = MPP_MODEL_TD / mpp
 
-        image_or = slide.get_thumbnail((w_l0 // reduction_factor, h_l0 // reduction_factor))
-        image_or.save(tis_det_dir_thumb + slide_name + ".jpg", quality = 80)
+        image_or = slide.get_thumbnail(
+            (w_l0 // reduction_factor, h_l0 // reduction_factor)
+        )
+        image_or.save(tis_det_dir_thumb + slide_name + ".jpg", quality=80)
+    except Exception as e:
+        print(f"Exception with loading {slide_name}: {e}")
+        exit(1)
 
-        '''
+    try:
+
+        """
         As tissue detector was trained on jpeg compressed images - we have to reproduce this step.
         Otherwise it functions suboptimal.
-        '''
+        """
 
         image = np.array(image_or)
         encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 80]
-        result, image = cv2.imencode('.jpg', image, encode_param)
+        result, image = cv2.imencode(".jpg", image, encode_param)
         image = cv2.imdecode(image, 1)
         image = Image.fromarray(image)
 
@@ -104,27 +136,41 @@ for slide_name in slide_names:
         overhang_wi = width - wi_n * M_P_S_MODEL_TD
         overhang_he = height - he_n * M_P_S_MODEL_TD
 
-        print('Overhang (< 1 patch) for width and height: ', overhang_wi, ',', overhang_he)
+        print(
+            "Overhang (< 1 patch) for width and height: ", overhang_wi, ",", overhang_he
+        )
+
+    except Exception as e:
+        print(f"Exception with jpeg compression for {slide_name}: {e}")
+        exit(1)
+
+    try:
 
         p_s = M_P_S_MODEL_TD
 
         for h in range(he_n + 1):
             for w in range(wi_n + 1):
                 if w != wi_n and h != he_n:
-                    image_work = image.crop((w * p_s, h * p_s, (w + 1) * p_s, (h + 1) * p_s))
+                    image_work = image.crop(
+                        (w * p_s, h * p_s, (w + 1) * p_s, (h + 1) * p_s)
+                    )
                 elif w == wi_n and h != he_n:
-                    image_work = image.crop((width - p_s, h * p_s, width, (h + 1) * p_s))
+                    image_work = image.crop(
+                        (width - p_s, h * p_s, width, (h + 1) * p_s)
+                    )
                 elif w != wi_n and h == he_n:
-                    image_work = image.crop((w * p_s, height - p_s, (w + 1) * p_s, height))
+                    image_work = image.crop(
+                        (w * p_s, height - p_s, (w + 1) * p_s, height)
+                    )
                 else:
                     image_work = image.crop((width - p_s, height - p_s, width, height))
 
                 image_pre = get_preprocessing(image_work, preprocessing_fn)
                 x_tensor = torch.from_numpy(image_pre).to(DEVICE).unsqueeze(0)
                 predictions = model.predict(x_tensor)
-                predictions = (predictions.squeeze().cpu().numpy())
+                predictions = predictions.squeeze().cpu().numpy()
 
-                mask = np.argmax(predictions, axis=0).astype('int8')
+                mask = np.argmax(predictions, axis=0).astype("int8")
 
                 class_mask = make_class_map(mask, colors)
 
@@ -132,34 +178,52 @@ for slide_name in slide_names:
                     temp_image = mask
                     temp_image_class_map = class_mask
                 elif w == wi_n:
-                    mask = mask[:, p_s - overhang_wi:p_s]
+                    mask = mask[:, p_s - overhang_wi : p_s]
                     temp_image = np.concatenate((temp_image, mask), axis=1)
-                    class_mask = class_mask[:, p_s - overhang_wi:p_s, :]
-                    temp_image_class_map = np.concatenate((temp_image_class_map, class_mask), axis=1)
+                    class_mask = class_mask[:, p_s - overhang_wi : p_s, :]
+                    temp_image_class_map = np.concatenate(
+                        (temp_image_class_map, class_mask), axis=1
+                    )
                 else:
                     temp_image = np.concatenate((temp_image, mask), axis=1)
-                    temp_image_class_map = np.concatenate((temp_image_class_map, class_mask), axis=1)
+                    temp_image_class_map = np.concatenate(
+                        (temp_image_class_map, class_mask), axis=1
+                    )
             if h == 0:
                 end_image = temp_image
                 end_image_class_map = temp_image_class_map
             elif h == he_n:
-                temp_image = temp_image [p_s - overhang_he:p_s,]
+                temp_image = temp_image[p_s - overhang_he : p_s,]
                 end_image = np.concatenate((end_image, temp_image), axis=0)
-                temp_image_class_map = temp_image_class_map [p_s - overhang_he:p_s, :, :]
-                end_image_class_map = np.concatenate((end_image_class_map, temp_image_class_map), axis=0)
+                temp_image_class_map = temp_image_class_map[
+                    p_s - overhang_he : p_s, :, :
+                ]
+                end_image_class_map = np.concatenate(
+                    (end_image_class_map, temp_image_class_map), axis=0
+                )
             else:
                 end_image = np.concatenate((end_image, temp_image), axis=0)
-                end_image_class_map = np.concatenate((end_image_class_map, temp_image_class_map), axis=0)
+                end_image_class_map = np.concatenate(
+                    (end_image_class_map, temp_image_class_map), axis=0
+                )
 
-        Image.fromarray(end_image).save(os.path.join(tis_det_dir_mask, slide_name + '_MASK.png'))
-        Image.fromarray(end_image_class_map).save(os.path.join(tis_det_dir_mask_col, slide_name + '_MASK_COL.png'))
-        overlay = cv2.addWeighted(np.array(image), OVER_IMAGE, end_image_class_map, OVER_MASK, 0)
+    except Exception as e:
+        print(f"Exception with inference for {slide_name}: {e}")
+        exit(1)
+
+    try:
+
+        Image.fromarray(end_image).save(
+            os.path.join(tis_det_dir_mask, slide_name + "_MASK.png")
+        )
+        Image.fromarray(end_image_class_map).save(
+            os.path.join(tis_det_dir_mask_col, slide_name + "_MASK_COL.png")
+        )
+        overlay = cv2.addWeighted(
+            np.array(image), OVER_IMAGE, end_image_class_map, OVER_MASK, 0
+        )
         overlay = Image.fromarray(overlay)
-        overlay.save(os.path.join(tis_det_dir_over, slide_name + '_OVERLAY.jpg'))
-    except:
-        print("Exception with", slide_name)
-
-
-
-
-
+        overlay.save(os.path.join(tis_det_dir_over, slide_name + "_OVERLAY.jpg"))
+    except Exception as e:
+        print(f"Exception with saving outputs for {slide_name}: {e}")
+        exit(1)
